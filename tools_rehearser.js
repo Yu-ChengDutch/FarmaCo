5c19b6975561997fa1b8f887d13eed5d64fe2823
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-09-26 09:47:21 +0200

    a

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -314,7 +314,7 @@ function checkMnemonicAnswer() {
         console.log("- - > Checking mnemonic")
         console.log("- - > Right answer is: " + correct_answer)
 
-        if (correct_answer.includes(given_answer) || given_answer.toLowerCase() == correct_answer.toString().toLowerCase()) {
+        if (given_answer.length > 2 && (correct_answer.includes(given_answer) || given_answer.toLowerCase() == correct_answer.toString().toLowerCase())) {
 
             console.log("- - > Correct!")
 
