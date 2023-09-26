713efd20ed3fd7293e592593501974acda8259bd
Yu-ChengDutch <39351818+Yu-ChengDutch@users.noreply.github.com>
2023-09-26 22:53:12 +0200

    a

--- tools_rehearser.js
+++ tools_rehearser.js
@@ -387,7 +387,7 @@ function checkMnemonicAnswer() {
             document.getElementById('remark-card').innerText = "The right mnemonic is: " + correct_answer + ". You'll repeat this questions once more afterwards.";
 
             local_question_array = question_array;
-            local_question_array.splice(current_index + 3, 0, {"Question": ("This is a repeat question: " + question_array[current_index].Question), "Answer": question_array[current_index].Answer});
+            local_question_array.splice(parseInt(current_index) + 3, 0, {"Question": ("This is a repeat question: " + question_array[current_index].Question), "Answer": question_array[current_index].Answer});
 
             question_array = local_question_array;
             console.log(question_array);
